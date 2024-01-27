621af606644eab627403d75a9e051d1d7034e22d
Maksym Voloshyn <mv.webdew@gmail.com>
2024-01-27 22:31:12 +0200

    feat: add streams subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+  const source = 'files/fileToRead.txt';
+  const readableStream = fs.createReadStream(
+    path.join(path.dirname(fileURLToPath(import.meta.url)), source)
+  );
+
+  readableStream.on('data', (chunk) => stdout.write(chunk));
+  readableStream.on('end', () => stdout.write('\nFile reading completed!'));
+  readableStream.on('error', (err) => {
+    throw err;
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,38 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { stdin, stdout } from 'process';
+
+const exitHandler = (message) => {
+  stdout.write(`\n${message}`);
+  stdin.end();
+  process.exit(0);
+};
+
 const write = async () => {
-    // Write your code here 
+  const dest = 'files/fileToWrite.txt';
+  const writableStream = fs.createWriteStream(
+    path.join(path.dirname(fileURLToPath(import.meta.url)), dest)
+  );
+  const exitCommands = [':q', ':quit', ':exit'];
+
+  stdout.write(
+    `To terminate the program, enter ${exitCommands.join(
+      ' / '
+    )} and press Enter\nEnter you message: \n`
+  );
+
+  stdin.on('data', (line) => {
+    if (exitCommands.includes(line.toString().trim())) {
+      return exitHandler('See you later!');
+    }
+
+    writableStream.write(line);
+  });
+
+  process.on('SIGINT', () =>
+    exitHandler('Oops! Forced termination, last input not recorded! ')
+  );
 };
 
 await write();
