d317a8a8f2c0637bea66d246d2b57ef5eadd85cc
Maksym Voloshyn <mv.webdew@gmail.com>
2024-01-27 15:23:11 +0200

    feat: add hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import crypto from 'crypto';
+import fs from 'fs';
+import { stdout } from 'process';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const source = 'files/fileToCalculateHashFor.txt';
+  const hash = crypto.createHash('sha256');
+  const readableStream = fs.createReadStream(
+    path.join(path.dirname(fileURLToPath(import.meta.url)), source)
+  );
+
+  readableStream.on('data', (data) => {
+    hash.update(data);
+  });
+
+  readableStream.on('end', () => {
+    stdout.write(`SHA-256 hash: ${hash.digest('hex')}`);
+  });
+
+  readableStream.on('error', (err) => {
+    throw err;
+  });
 };
 
 await calculateHash();
