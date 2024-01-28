7bbfaa3bbdf904c02228d59a83f68d34c3bdf27e
Maksym Voloshyn <mv.webdew@gmail.com>
2024-01-28 21:25:08 +0200

    fix: expand CustomError

--- src/utils/customError.js
+++ src/utils/customError.js
@@ -1,6 +1,6 @@
 class CustomError extends Error {
-  constructor({ name, path, description }) {
-    super('FS operation failed');
+  constructor({ name, path, description, message }) {
+    super(message || 'FS operation failed');
     this.name = name;
     this.path = path;
     this.description = description;
