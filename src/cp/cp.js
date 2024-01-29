6bde5fc74cb39b89ced6039e8cb0ba99642a2874
Maksym Voloshyn <mv.webdew@gmail.com>
2024-01-29 18:37:14 +0200

    feat: add CP subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,38 @@
+import path from 'path';
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathToScript = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    'files',
+    'script.js'
+  );
+  const childProcess = fork(pathToScript, args, {
+    stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+  });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+
+  childProcess
+    .on('exit', (code) => {
+      console.log(`Child process exit with code: ${code}`);
+    })
+    .on('error', (err) => {
+      console.error(`Error: ${err.message}`);
+    });
+
+  process.stdin
+    .on('end', () => {
+      console.log('sdfdsfsdfsfd');
+      childProcess.stdin.end();
+    })
+    .on('error', (err) => {
+      console.error(`Error: ${err.message}`);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(
+  process.argv.slice(2).length ? process.argv.slice(2) : ['arg1', 'arg2']
+);
